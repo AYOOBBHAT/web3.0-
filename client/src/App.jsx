ab415230d4da8eca534dc5175d556c59439e93c2
Ayoob Bhat <ayoob324005@gmail.com>
2023-11-10 16:42:45 +0530

    added componemts

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+
+import { Navbar,Welcome,Footer,Transactions,Loader,Services } from './components'
+
+import { useState } from 'react'
+
+const  App=()=> {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar/>
+      <Welcome/>
+
+    </div>
+    <Services/>
+    <Transactions/>
+    <Footer/>
+   </div>
+     )
+}
+
+export default App
